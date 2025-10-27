dd4de7061f2d0760e31907a8551c81b7955dd96b
apiload5 <amir03182055632@gmail.com>
2025-10-27 18:17:58 +0500

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file, render_template_string
+Failed to fetch error araha he "from flask import Flask, request, jsonify, send_file, render_template_string
 from flask_cors import CORS
 import yt_dlp
 import os
@@ -22,6 +22,16 @@ app = Flask(__name__)
 application = app
 
 # --- SECURITY: CORS CONFIGURATION ---
+ALLOWED_ORIGINS = [
+    # 1.  naya aur main secure frontend domain (Recommended)
+    "https://savemedia.online",
+    
+    # 2.  naya secure backend subdomain (agar direct request aati hai)
+    "https://backend.savemedia.online",
+    
+    # 3. Purana Blogger domain (Agar abhi bhi test kar rahe hain)
+    "https://crispy0921.blogspot.com"
+]
 
 # Allow all specified origins
 CORS(app, origins=ALLOWED_ORIGINS, supports_credentials=True)
