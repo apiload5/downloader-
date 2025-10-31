5689caf37480a337ab6567adabf1af8e2c42376f
apiload5 <amir03182055632@gmail.com>
2025-10-31 16:58:28 +0500

    Update application.py

--- application.py
+++ application.py
@@ -213,5 +213,6 @@ async def download(req: Request, url: str = "", format_id: Optional[str] = None)
 # Run Locally or on Replit
 # ------------------------------
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
+    import uvicorn, os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
